3882566c617bb6b68276719ddfa4ff9f5d1450a7
Gurjeet <gurgrewal143@gmail.com>
2024-09-29 13:26:49 +0530

    custombutton improved

--- components/CustomButton.jsx
+++ components/CustomButton.jsx
@@ -5,7 +5,7 @@ const CustomButton = ({ variant, title, frontIcon, backIcon, type, loading, ...p
     return (
         <button disabled={loading} type={type || "button"} {...props} className={`text-sm font-semibold  border border-green-800/20 space-x-2 flex items-center px-3 rounded-xl cursor-pointer gap-2 ${variant == "primary" ? 'bg-green-700 hover:bg-green-800 text-white py-[7px] ' : 'py-[5px] hover:bg-primary text-green-700'}`}>
             {
-                loading ? <CircularProgress size={30} color={variant == "primary" ? "white" : "black"} /> : <>
+                loading ? <><CircularProgress size={15} color={variant == "primary" ? "white" : "black"} /> Please Wait...</> : <>
                     {frontIcon}
                     {title}
                     {backIcon}
