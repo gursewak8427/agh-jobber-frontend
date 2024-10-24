0e4a367d8bce556df2cb56b1e2ab1a7a02b397a9
Barinderpreet Singh <naffydharni006@gmail.com>
2024-10-24 15:55:58 +0000

    complete company section with one bug

--- app/(dashboard)/settings/company/page.jsx
+++ app/(dashboard)/settings/company/page.jsx
@@ -1,5 +1,56 @@
 import { Input } from "../_components/input";
 import { Label } from "../_components/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../_components/select";
+
+const countries = [
+  { id: "AF", name: "Afghanistan" },
+  { id: "AX", name: "Aland Islands" },
+  { id: "AL", name: "Albania" },
+  { id: "DZ", name: "Algeria" },
+  { id: "AS", name: "American Samoa" },
+  { id: "AD", name: "Andorra" },
+  { id: "AO", name: "Angola" },
+  { id: "AI", name: "Anguilla" },
+  { id: "AQ", name: "Antarctica" },
+  { id: "AG", name: "Antigua and Barbuda" },
+  { id: "AR", name: "Argentina" },
+  { id: "AM", name: "Armenia" },
+  { id: "AW", name: "Aruba" },
+  { id: "AU", name: "Australia" },
+  { id: "AT", name: "Austria" },
+  { id: "AZ", name: "Azerbaijan" },
+  { id: "BS", name: "Bahamas" },
+  { id: "BH", name: "Bahrain" },
+  { id: "BD", name: "Bangladesh" },
+  { id: "BB", name: "Barbados" },
+  { id: "BY", name: "Belarus" },
+  { id: "BE", name: "Belgium" },
+  { id: "BZ", name: "Belize" },
+  { id: "BJ", name: "Benin" },
+  { id: "BM", name: "Bermuda" },
+  { id: "BT", name: "Bhutan" },
+  { id: "BO", name: "Bolivia" },
+  { id: "BQ", name: "Bonaire, Sint Eustatius and Saba" },
+  { id: "BA", name: "Bosnia and Herzegovina" },
+  { id: "BW", name: "Botswana" },
+  { id: "BV", name: "Bouvet Island" },
+  { id: "BR", name: "Brazil" },
+  { id: "IO", name: "British Indian Ocean Territory" },
+  { id: "BN", name: "Brunei Darussalam" },
+  { id: "BG", name: "Bulgaria" },
+  { id: "BF", name: "Burkina Faso" },
+  { id: "BI", name: "Burundi" },
+  { id: "KH", name: "Cambodia" },
+  { id: "CM", name: "Cameroon" },
+  { id: "CA", name: "Canada" },
+  { id: "CV", name: "Cape Verde" },
+];
 
 export default function CompanySetting() {
   return (
@@ -39,18 +90,22 @@ export default function CompanySetting() {
           <div className="grid md:grid-cols-2">
             <Input
               id="street-1"
-              className="md:col-span-2 rounded-b-none"
+              className="md:col-span-2 rounded-b-none border-b-0"
               placeholder="Street 1"
             />
             <Input
               id="street-2"
-              className="md:col-span-2 rounded-none"
+              className="md:col-span-2 rounded-none border-b-0"
               placeholder="Street 2"
             />
-            <Input id="city" className="rounded-none" placeholder="City" />
+            <Input
+              id="city"
+              className="rounded-none border-b-0"
+              placeholder="City"
+            />
             <Input
               id="province"
-              className="rounded-none"
+              className="rounded-none border-b-0 border-l-0"
               placeholder="Province"
             />
             <Input
@@ -58,17 +113,28 @@ export default function CompanySetting() {
               className="rounded-none rounded-bl-md"
               placeholder="Postal Code"
             />
-            <Input
-              id="country"
-              className="rounded-none rounded-br-md"
-              placeholder="Country"
-              type="select"
-            />
+            <Select defaultValue="india">
+              <SelectTrigger className="w-[180px] rounded-t-none rounded-bl-none border-l-0">
+                <SelectValue placeholder="Country" />
+              </SelectTrigger>
+              <SelectContent>
+                {countries.map((country) => (
+                  <SelectItem key={country.id} value={country.id}>
+                    {country.name}
+                  </SelectItem>
+                ))}
+                <SelectItem value="india">India</SelectItem>
+                <SelectItem value="pakistan">Pakistan</SelectItem>
+                <SelectItem value="weird-one">
+                  I don't like the country system
+                </SelectItem>
+              </SelectContent>
+            </Select>
           </div>
         </div>
       </div>
       <div className="w-full max-w-4xl shadow-md rounded-lg p-6 border border-[#9ca3af] space-y-4 animate-fadeIn">
-      <h3 className="text-2xl font-bold">Business Hours</h3>
+        <h3 className="text-2xl font-bold">Business Hours</h3>
       </div>
     </div>
   );
